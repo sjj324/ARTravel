b169eb02f7c09e844c87940e9d73cd6ba5a871cf
sjj324 <yongjae.link@gmail.com>
2020-06-18 14:52:11 +0900

    Project Upload

---
+++ Assets/Scripts/Click.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour
+{
+    public GameObject Canvas1;
+    public GameObject Canvas2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hitinfo = new RaycastHit();
+
+            if (Physics.Raycast(ray, out hitinfo, 100, 1<<9))
+            {
+                Destroy(GameObject.Find("Sign_Left_Turn_Only"));
+                //gameObject.GetComponent<Transform>().localPosition = new Vector3(0.195f, 0, -0.6f); n
+                Canvas1.SetActive(true);
+            }
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hitinfo = new RaycastHit();
+
+            if (Physics.Raycast(ray, out hitinfo, 100, 1 << 10))
+            {
+                Destroy(GameObject.Find("Sign_Right_Turn_Only"));
+                Canvas2.SetActive(true);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    private Camera arCamera;
+    private RaycastHit hit;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        arCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // BT350에서는 터치를 의미한다.
+        {
+            Debug.Log("Touch");
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            //Debug.Log("Touching" + Time.time);
+            Debug.Log($"Touching {Time.time}");
+        }
+        //터치 좌표값
+        Vector2 pos = Input.mousePosition;
+        //스크린 터치 좌표값을 3차원 Ray로 생성
+        Ray ray = arCamera.ScreenPointToRay(pos);
+
+        if (Input.GetMouseButtonDown(0)
+            && Physics.Raycast(ray, out hit, 10.0f, 1<<8))
+        {
+            hit.collider.GetComponent<Renderer>().material.color = Color.red;
+        }
+
+        //화살표 입력값
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Debug.Log("Click Left Arrow");
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Debug.Log("Click Right Arrow");
+        }
+    }
+
+}
